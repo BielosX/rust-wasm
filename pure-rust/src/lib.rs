cd1504afa535439c92e75947dfb040d0c56afaf9
Mateusz Biel <matbiel@gmail.com>
2024-09-20 09:20:58 +0200

    Remove redundant type declarations

--- pure-rust/src/lib.rs
+++ pure-rust/src/lib.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::slice::{from_raw_parts};
+use std::slice::from_raw_parts;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -30,12 +30,12 @@ pub extern "C" fn to_lower(offset: i32, length: i32) {
 pub extern "C" fn user_stringify(offset: i32, length: i32) -> (i32, i32) {
     unsafe {
         let len = length as usize;
-        let ptr: *mut u8 = offset as *mut u8;
+        let ptr = offset as *mut u8;
         let json = from_raw_parts(ptr, len);
         let user: User = serde_json::from_slice(json).unwrap();
         let result = String::from(format!("{} {}", user.first_name, user.last_name));
         let result_offset = 20000;
-        let result_ptr: *mut u8 = result_offset as *mut u8;
+        let result_ptr = result_offset as *mut u8;
         let bytes = result.into_bytes();
         let bytes_len = bytes.len();
         for n in 0..bytes_len {
