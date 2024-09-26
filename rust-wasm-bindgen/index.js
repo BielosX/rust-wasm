244d2078cfab2ac9ec3f0be80b85618c27b55f25
Mateusz Biel <matbiel@gmail.com>
2024-09-26 19:35:07 +0200

    Implement form submit handler

--- rust-wasm-bindgen/index.js
+++ rust-wasm-bindgen/index.js
@@ -1,5 +1,7 @@
 import init_wasm, {greet, init} from './pkg/rust_wasm_bindgen.js';
 
+let state;
+
 init_wasm().then(() => {
     init();
     greet();
--- rust-wasm-bindgen/src/lib.rs
+++ rust-wasm-bindgen/src/lib.rs
@@ -1,6 +1,36 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use web_sys::wasm_bindgen::JsCast;
+use web_sys::{Document, FormData, HtmlFormElement, SubmitEvent};
+
+trait Speak {
+    fn speak(&self) -> String;
+}
+
+struct Cat {
+    age: u32,
+    name: String,
+}
+
+struct Dog {
+    age: u32,
+    name: String,
+}
+
+impl Speak for Cat {
+    fn speak(&self) -> String {
+        format!("MEOW! Name: {}, Age: {}\n", self.name, self.age)
+    }
+}
+
+impl Speak for Dog {
+    fn speak(&self) -> String {
+        format!("BARK! Name: {}, Age: {}\n", self.name, self.age)
+    }
+}
+
+static mut ANIMALS: Vec<Box<dyn Speak>> = Vec::new();
 
 #[wasm_bindgen]
 extern "C" {
@@ -8,9 +38,69 @@ extern "C" {
     fn log(s: &str);
 }
 
+fn add_animal(name: &String, age: &String, animal_type: &String) {
+    match animal_type.as_str() {
+        "cat" => unsafe {
+            ANIMALS.push(Box::new(Cat {
+                name: name.clone(),
+                age: age.parse::<u32>().unwrap(),
+            }));
+        },
+        "dog" => unsafe {
+            ANIMALS.push(Box::new(Dog {
+                name: name.clone(),
+                age: age.parse::<u32>().unwrap(),
+            }));
+        },
+        _ => {}
+    }
+}
+
+unsafe fn describe_animals() -> String {
+    let mut result = String::new();
+    ANIMALS
+        .iter()
+        .for_each(|item| result.push_str(item.speak().as_str()));
+    result
+}
+
+unsafe fn handle_submit_event(document: &Document, html_element: &HtmlFormElement, event: SubmitEvent) {
+    let form_data = FormData::new_with_form(html_element).unwrap();
+    let name = form_data.get("animal-name").as_string().unwrap();
+    let age = form_data.get("animal-age").as_string().unwrap();
+    let animal_type = form_data.get("animal-type").as_string().unwrap();
+    add_animal(&name, &age, &animal_type);
+    let age_element = document.get_element_by_id("animal-count").unwrap();
+    let speak_element = document.get_element_by_id("animal-speak").unwrap();
+    age_element.set_text_content(Some(ANIMALS.len().to_string().as_str()));
+    let animals = describe_animals();
+    speak_element.set_text_content(Some(animals.as_str()));
+    event.prevent_default();
+}
+
+unsafe fn setup_animal_form(document: &Document) {
+    let doc = document.clone();
+    let form = document
+        .get_element_by_id("animal-form")
+        .expect("animal-form should be available");
+    let form_html_element = form.dyn_ref::<HtmlFormElement>().unwrap().clone();
+    let callback = Closure::<dyn FnMut(_)>::new(move |event| {
+        handle_submit_event(&doc, &form_html_element, event);
+    });
+    form.add_event_listener_with_callback("submit", callback.as_ref().unchecked_ref())
+        .expect("Submit callback should be added");
+    callback.forget();
+}
+
 #[wasm_bindgen]
 pub fn init() {
     utils::set_panic_hook();
+    let window = web_sys::window().expect("Window should be available");
+    let document = window.document().expect("Document should be available");
+
+    unsafe {
+        setup_animal_form(&document);
+    }
 }
 
 #[wasm_bindgen]
